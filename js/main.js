b0ba9c1a05516d5ff51c91713faadd7e9d7c1ec8
erinmilling <mill0814@algonquinlive.com>
2016-03-11 09:45:59 -0500

    added some stuff

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+
 $header.addClass('masthead');
+$h1.addClass('logo');
+$h1.removeClass('big-heading');
+$main.addClass('wrapper');
+$main: first-child.addClass('intro');
+
+.remove(big-heading);
+
+$('ul :first-child');
+.slug-list-first'
